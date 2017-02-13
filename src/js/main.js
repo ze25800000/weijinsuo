b6f6cf580768378ee56ed45b905570f4b3bae166
ze25800000 <ze258100000@sina.com>
2017-02-13 10:30:40 +0800

    update

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    function resize() {
+        console.log('onresize');
+        var windowWidth = $(window).width();
+        var isSmallScreen = windowWidth < 768;
+        $('#ad > .carousel-inner > .item').each(function (i, item) {
+            // 因为拿到是DOM对象 需要转换
+            var $item = $(item);
+            // var imgSrc = $item.data(isSmallScreen ? 'image-xs' : 'image-lg');
+            var imgSrc =isSmallScreen ? $item.data('image-xs') : $item.data('image-lg');
+            console.log(imgSrc);
+            // 设置背景图片
+            $item.css('backgroundImage', 'url("' + imgSrc + '")');
+            //
+            // 因为我们需要小图时 尺寸等比例变化，所以小图时我们使用img方式
+            if (isSmallScreen) {
+                $item.html('<img src="' + imgSrc + '" alt="" />');
+            } else {
+                $item.empty();
+            }
+        });
+    }
+    resize();
+    window.onresize = resize;
+});
